751b5b9a32536ac9c15091640deb35ef9a780bd7
Aditya Bhusal <121667136+AdityaBhusal@users.noreply.github.com>
2025-07-19 23:43:15 +0545

    Add files via upload

---
+++ Gamestore.Frontend/Clients/GenresClient.cs
@@ -0,0 +1,9 @@
+﻿using Gamestore.Frontend.Models;
+
+namespace Gamestore.Frontend.Clients;
+
+public class GenresClient(HttpClient httpClient)
+{    public async Task<Genre[]> GetGenresAsync() 
+    => await httpClient.GetFromJsonAsync<Genre[]>("genres") ?? [];
+    
+}
